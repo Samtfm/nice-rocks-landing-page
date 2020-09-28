9565afaa3b5ac661a87f5f2d6987dae31d543779
Samtfm <sfmemail@gmail.com>
2020-09-28 19:40:23 -0400

    Updates

---
+++ precache-manifest.2d54c16f4aaf6b93c8f34d2f0e1e8116.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff067c36548854916e59552ff159abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0171926cf708ff9dead",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff61502f808cb130f5c2",
+    "url": "/static/js/2.758cde11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.758cde11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0171926cf708ff9dead",
+    "url": "/static/js/main.d77c1f46.chunk.js"
+  },
+  {
+    "revision": "2c3a5f0e26fa367d58f7",
+    "url": "/static/js/runtime-main.1be3993f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nice-rocks-landing-page/precache-manifest.734af409ca2c160587cf117da2f2e93e.js"
+  "/precache-manifest.2d54c16f4aaf6b93c8f34d2f0e1e8116.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nice-rocks-landing-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
