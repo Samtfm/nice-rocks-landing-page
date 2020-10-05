f05ca388ff8d8224e862e36bf1d7e1a7bc877b7d
Samtfm <sfmemail@gmail.com>
2020-10-05 11:18:17 -0400

    add text and images to info sections

--- src/App.js
+++ src/App.js
@@ -39,27 +39,42 @@ function App() {
             },
           ]}
           screenshot={"https://placekitten.com/250/450"}
-          graphic={"https://placekitten.com/120/80"}
+          graphic={"./images/bird.svg"}
         />
         <InfoSection 
-          title={"There, for you"}
+          title={"No time pressure"}
           paragraphs={[
             {
-              title: "Favorite things",
-              body: "A Rock is a recommendation to you, for something you might enjoy.  It could be music, books, articles, or really anything.",
+              title: "Customizable notification settings",
+              body: "Choose times you want to be notified about new content. You can be alerted to new recommendations right when your workday ends and your leisure time begins.",
             },
             {
-              title: "Personal content by design",
-              body: 'There are no algorithmically generated feeds or influencer accounts to follow. The only recomendations you see will be made personally to you, by your friends.',
+              title: "Catch up at your leisure",
+              body: "Rocks stay in your collection until you have the time for them. No more scrolling back through long chat threads to find the name of that movie you’ve been meaning to see.",
             },
             {
-              title: "Resharable",
-              body: "Re-sharing a Rock that was sent to you is easy. Just tap the arrow icon and add a new note to your friend.",
+              title: "Send a response",
+              body: "Once you’re finished, send a response! Let your friend know if your thoughts, and the rock will move from your collection to your archive. Out of the way, but there if you want to revisit it.",
             },
           ]}
           screenshot={"https://placekitten.com/250/450"}
-          graphic={"https://placekitten.com/120/80"}
+          graphic={"./images/rocks.svg"}
           horizontalFlip
+        />
+        <InfoSection 
+          title={"Secure and reliable"}
+          paragraphs={[
+            {
+              title: "Never lose a thing",
+              body: "Nice rocks is built on Google Firebase, so all your data is stored on Google’s cloud servers.",
+            },
+            {
+              title: "Offline mode",
+              body: "Access your collection without an internet connection. You can even send rocks and responses, which will queue in the background and then send as soon as you reconnect.",
+            },
+          ]}
+          screenshot={"https://placekitten.com/250/450"}
+          graphic={"./images/leaves.svg"}
         />
       </div>
     </ThemeProvider>
